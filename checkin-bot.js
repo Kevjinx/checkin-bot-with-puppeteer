a0124b76396fbf33095a3fff43e6a3bb485c63b5
kevjinx <kevin415mei@gmail.com>
2021-06-28 07:47:33 -0700

    working checkin bot

---
+++ checkin-bot.js
@@ -0,0 +1,44 @@
+const puppeteer = require('puppeteer');
+const dayjs = require('dayjs')
+require('dotenv').config();
+const email = process.env.GITHUB_EMAIL.toString()
+const password = process.env.GITHUB_PASSWORD.toString()
+
+const delay = (time) => {
+  return new Promise(function(resolve) {
+      setTimeout(resolve, time)
+  });
+}
+
+const checkIn = async () => {
+  console.log('start');
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('https://progress.appacademy.io/students/auth/github')
+  console.log('signing up');
+  await page.type('#login_field', email)
+  await page.type('#password', password)
+  await page.keyboard.press('Enter');
+  console.log('signed in');
+  await page.waitForNavigation(),
+  await delay(5000)
+  await page.waitForSelector('.zoom-github-links'),
+  console.log('logged in');
+
+  let i = 0
+  setInterval(async () => {
+    i++
+    console.log(`Looking for the checkIn button, at ${dayjs().format('hh:mm:ss A')} for the ${i}th time`);
+    try {
+      if (await page.$('.checkin')) {
+        await page.click('.checkin')
+        let now = dayjs().format('hh:mm:ss A')
+        console.log(`checked in at ${now}`);
+        await page.screenshot({ path: `./screenshot/checkInAt${dayjs().format('hh:mm:ss')}.png`})
+      }
+    } catch(e) {}
+  }, 120000);
+
+}
+
+checkIn()
